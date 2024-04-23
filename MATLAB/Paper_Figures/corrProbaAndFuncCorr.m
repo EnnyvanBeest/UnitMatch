6ecee6a19e0f866e3a5f1a7e0e7d5ba273322a0e
Célian Bimbard <54884591+cbimbo@users.noreply.github.com>
2024-04-23 19:01:46 +0100

    Update corrProbaAndFuncCorr.m

--- MATLAB/Paper_Figures/corrProbaAndFuncCorr.m
+++ MATLAB/Paper_Figures/corrProbaAndFuncCorr.m
@@ -15,15 +15,15 @@ MatchTable.ISICorrID2 = mat2vec(repmat(tmp,[1,sqrt(size(MatchTable,1))]));
 
 %%
 
-FPNames = {'ISICorr','natImRespCorr','refPopCorr'};
+FPNames = {'ISICorr','refPopCorr','natImRespCorr'};
 
-probeBins = [0:0.05:1]; %[0 10.^(-7:0.2:0)]; %
-probeBins(end) = 1.01; % to include p=1 in last bin
-x = .5*probeBins(1:end-1)+.5*probeBins(2:end);
+probaBins = [0:0.05:1]; %[0 10.^(-7:0.2:0)]; %
+probaBins(end) = 1.01; % to include p=1 in last bin
+x = .5*probaBins(1:end-1)+.5*probaBins(2:end);
 
-% whichID = '';
+whichID = '';
 % whichID = 'Liberal';
-whichID = 'Conservative';
+% whichID = 'Conservative';
 
 matchIdx = MatchTable.(['UID1' whichID]) == MatchTable.(['UID2' whichID]);
 % matchIdx = MatchTable.MatchProb*0.5+MatchTable.FlipMatchProb*0.5 > 0.5; 
@@ -36,18 +36,18 @@ splitUnitsIdx = ismember(MatchTable.(['UID1' whichID]),splitUnitsUIDs);
 figure('Position',[809   595   700   420]);
 for fpIdx = 1:numel(FPNames)
     FPNameCurr = FPNames{fpIdx};
-    y = nan(1, numel(probeBins)-1);
-    err = nan(1, numel(probeBins)-1);
-    n = nan(1, numel(probeBins)-1);
-    y_match = nan(1, numel(probeBins)-1);
-    err_match = nan(1, numel(probeBins)-1);
-    n_match = nan(1, numel(probeBins)-1);
-    y_nonmatch = nan(1, numel(probeBins)-1);
-    err_nonmatch = nan(1, numel(probeBins)-1);
-    n_nonmatch = nan(1, numel(probeBins)-1);
-    for pb = 1:numel(probeBins)-1
-        idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2; % & ... 
-            % splitUnitsIdx;% & ...
+    y = nan(1, numel(probaBins)-1);
+    err = nan(1, numel(probaBins)-1);
+    n = nan(1, numel(probaBins)-1);
+    y_match = nan(1, numel(probaBins)-1);
+    err_match = nan(1, numel(probaBins)-1);
+    n_match = nan(1, numel(probaBins)-1);
+    y_nonmatch = nan(1, numel(probaBins)-1);
+    err_nonmatch = nan(1, numel(probaBins)-1);
+    n_nonmatch = nan(1, numel(probaBins)-1);
+    for pb = 1:numel(probaBins)-1
+        idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ... 
+            splitUnitsIdx;% & ...
             % MatchTable.EucledianDistance < 10;
         % all
         n(pb) = sum(~isnan(MatchTable.(FPNameCurr)(idx)));
@@ -91,8 +91,8 @@ end
 clear AUC
 for fpIdx = 1:numel(FPNames)
     FPNameCurr = FPNames{fpIdx};
-    for pb = 1:numel(probeBins)-1
-        idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2; % & ... 
+    for pb = 1:numel(probaBins)-1
+        idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2; % & ... 
             % splitUnitsIdx;% & ...
             % MatchTable.EucledianDistance < 10;
         
@@ -116,22 +116,22 @@ ylabel('AUC')
 figure; hold all
 % histogram(MatchTable(matchIdx & ~splitUnitsIdx,:).ISICorr,-1:0.05:1,'Normalization','probability')
 pb = 1;
-idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
+idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
             MatchTable.EucledianDistance < 10;
 histogram(MatchTable(idx & ~matchIdx & ~splitUnitsIdx,:).ISICorr,-1:0.05:1,'Normalization','probability')
-pb = numel(probeBins)-1;
-idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
+pb = numel(probaBins)-1;
+idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
             MatchTable.EucledianDistance < 10;
 histogram(MatchTable(idx & ~matchIdx & ~splitUnitsIdx,:).ISICorr,-1:0.05:1,'Normalization','probability')
 
 %%
 figure; hold all
 pb = 1;
-idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
+idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
             MatchTable.EucledianDistance < 10;
 histogram(MatchTable(idx & ~matchIdx & ~splitUnitsIdx,:).FlipMatchProb,0:0.03:1,'Normalization','probability')
 pb = 2;
-idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
+idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
             MatchTable.EucledianDistance < 10;
 histogram(MatchTable(idx & ~matchIdx & ~splitUnitsIdx,:).FlipMatchProb,0:0.03:1,'Normalization','probability')
 
@@ -143,14 +143,16 @@ histogram(MatchTable(idx & ~matchIdx & ~splitUnitsIdx,:).FlipMatchProb,0:0.03:1,
 fpIdx = 1;
 FPNameCurr = FPNames{fpIdx};
 corrBins = -1:0.1:5;
-trackedTrueDistr = histcounts(atanh(MatchTable(matchIdx & ~splitUnitsIdx & MatchTable.MatchProb > 0.99 & MatchTable.RecSes1 ~= MatchTable.RecSes2,:).(FPNameCurr)),corrBins,'Normalization','probability');
-differentTrueDistr = histcounts(atanh(MatchTable(~matchIdx & ~splitUnitsIdx & MatchTable.MatchProb < 1e-5 & MatchTable.RecSes1 ~= MatchTable.RecSes2,:).(FPNameCurr)),corrBins,'Normalization','probability');
+trackedTrueIdx = matchIdx & ~splitUnitsIdx & MatchTable.MatchProb > 0.99 & MatchTable.RecSes1 ~= MatchTable.RecSes2;
+trackedTrueDistr = histcounts(atanh(MatchTable(trackedTrueIdx,:).(FPNameCurr)),corrBins,'Normalization','probability');
+differentTrueIdx = ~matchIdx & ~splitUnitsIdx & MatchTable.MatchProb < 1e-5 & MatchTable.RecSes1 ~= MatchTable.RecSes2;
+differentTrueDistr = histcounts(atanh(MatchTable(differentTrueIdx,:).(FPNameCurr)),corrBins,'Normalization','probability');
 
 X = [trackedTrueDistr; differentTrueDistr];
 
-%% average case --  all tracked
+%% average case 1 --  all tracked
 
-actualDistr = histcounts(atanh(MatchTable(matchIdx,:).(FPNameCurr)),corrBins,'Normalization','probability');
+actualDistr = histcounts(atanh(MatchTable(matchIdx & ~splitUnitsIdx & MatchTable.RecSes1 ~= MatchTable.RecSes2,:).(FPNameCurr)),corrBins,'Normalization','probability');
 
 b = X'\actualDistr';
 
@@ -160,9 +162,9 @@ stairs(corrBins(1:end-1), differentTrueDistr, 'b')
 stairs(corrBins(1:end-1), actualDistr, 'k')
 stairs(corrBins(1:end-1), X'*b, 'Color', [.5 .5 .5])
 
-%% average case --  all different
+%% average case 2 --  all different
 
-actualDistr = histcounts(atanh(MatchTable(~matchIdx,:).(FPNameCurr)),corrBins,'Normalization','probability');
+actualDistr = histcounts(atanh(MatchTable(~matchIdx & ~splitUnitsIdx & MatchTable.RecSes1 ~= MatchTable.RecSes2,:).(FPNameCurr)),corrBins,'Normalization','probability');
 
 b = X'\actualDistr';
 
@@ -175,8 +177,8 @@ stairs(corrBins(1:end-1), X'*b, 'Color', [.5 .5 .5])
 %% extreme case 1 -- prob = 0, tracked
 
 pb = 1;
-idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2;
-actualDistr = histcounts(atanh(MatchTable(idx & matchIdx,:).(FPNameCurr)),corrBins,'Normalization','probability');
+idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & matchIdx & ~splitUnitsIdx ;
+actualDistr = histcounts(atanh(MatchTable(idx,:).(FPNameCurr)),corrBins,'Normalization','probability');
 
 b = X'\actualDistr';
 
@@ -186,11 +188,16 @@ stairs(corrBins(1:end-1), differentTrueDistr, 'b')
 stairs(corrBins(1:end-1), actualDistr, 'k')
 stairs(corrBins(1:end-1), X'*b, 'Color', [.5 .5 .5])
 
-%% extreme case 1 -- prob = 1, different
+figure;
+histogram(abs(MatchTable(differentTrueIdx,:).RecSes2 - MatchTable(differentTrueIdx,:).RecSes1),'Normalization','probability')
+hold all
+histogram(abs(MatchTable(idx,:).RecSes2 - MatchTable(idx,:).RecSes1),'Normalization','probability')
 
-pb = numel(probeBins)-1;
-idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2;
-actualDistr = histcounts(atanh(MatchTable(idx & ~matchIdx,:).(FPNameCurr)),corrBins,'Normalization','probability');
+%% extreme case 2 -- prob = 1, different
+
+pb = numel(probaBins)-1;
+idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ~matchIdx & ~splitUnitsIdx;
+actualDistr = histcounts(atanh(MatchTable(idx,:).(FPNameCurr)),corrBins,'Normalization','probability');
 
 b = X'\actualDistr';
 
@@ -199,13 +206,18 @@ stairs(corrBins(1:end-1), trackedTrueDistr, 'r')
 stairs(corrBins(1:end-1), differentTrueDistr, 'b')
 stairs(corrBins(1:end-1), actualDistr, 'k')
 stairs(corrBins(1:end-1), X'*b, 'Color', [.5 .5 .5])
+
+figure;
+histogram(abs(MatchTable(trackedTrueIdx,:).RecSes2 - MatchTable(trackedTrueIdx,:).RecSes1),'Normalization','probability')
+hold all
+histogram(abs(MatchTable(idx,:).RecSes2 - MatchTable(idx,:).RecSes1),'Normalization','probability')
 
 %% across all proba bins
 
 fpIdx = 1;
-for pb = 1:numel(probeBins)-1
-    idx = MatchTable.MatchProb >= probeBins(pb) & MatchTable.MatchProb < probeBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2; % & ...
-    % splitUnitsIdx;% & ...
+for pb = 1:numel(probaBins)-1
+    idx = MatchTable.MatchProb >= probaBins(pb) & MatchTable.MatchProb < probaBins(pb+1) & MatchTable.RecSes1 ~= MatchTable.RecSes2 & ...
+    splitUnitsIdx;% & ...
     % MatchTable.EucledianDistance < 10;
     % all
     actualDistr = histcounts(atanh(MatchTable(idx,:).(FPNameCurr)),corrBins,'Normalization','probability');
@@ -221,3 +233,23 @@ for pb = 1:numel(probeBins)-1
     n_nonmatch_TN(pb) = sum(idx & ~matchIdx) * b_nonmatch(2,pb);
     n_nonmatch_FN(pb) = sum(idx & ~matchIdx) * b_nonmatch(1,pb);
 end
+
+figure;
+subplot(131); hold all
+stairs(probaBins(1:end-1), b_all(1,:),'r')
+stairs(probaBins(1:end-1), b_all(2,:),'b')
+legend({'same','different'})
+xlabel('Match probability')
+ylabel('%')
+title('all')
+ylim([0 1])
+subplot(132); hold all
+stairs(probaBins(1:end-1), b_match(1,:),'r')
+stairs(probaBins(1:end-1), b_match(2,:),'b')
+title('tracked')
+ylim([0 1])
+subplot(133); hold all
+stairs(probaBins(1:end-1), b_nonmatch(1,:),'r')
+stairs(probaBins(1:end-1), b_nonmatch(2,:),'b')
+title('different')
+ylim([0 1])
