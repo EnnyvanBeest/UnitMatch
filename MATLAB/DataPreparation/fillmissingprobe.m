e7b63cd9ab4b7dc8705931a387075592d80fe96c
Enny H. van Beest <ennyvanbeest@gmail.com>
2024-05-09 18:22:16 +0100

    Fillmissing probe
    - different filling in

--- MATLAB/DataPreparation/fillmissingprobe.m
+++ MATLAB/DataPreparation/fillmissingprobe.m
@@ -1,45 +1,50 @@
 function channelpos = fillmissingprobe(channelpos)
 % Fill missing values of a neuropixels probe map
 
-% Extract unique values
-xUnique = unique(channelpos(:,1),'stable');
-yUnique = unique(channelpos(:,2),'stable');
-
+% Extract unique x values
+[xUnique,~,chOrdertmp] = unique(channelpos(~isnan(channelpos(:,1)),1),'stable');
 xUnique = xUnique(~isnan(xUnique));
-yUnique = yUnique(~isnan(yUnique));
 
-% Steps in y
-ySteps = unique(diff(yUnique));
+chOrder = nan(1,size(channelpos,1));
+chOrder(~isnan(channelpos(:,1))) = chOrdertmp;
 
-% identify nans
-rowId = find(isnan(channelpos(:,1)));
-for id = 1:numel(rowId)
-    % Take a few samples around the missing channel
-    TakeRows = rowId-length(xUnique)/2:rowId+length(xUnique)/2; 
-    TakeRows(TakeRows<1|TakeRows>size(channelpos,1)) = [];
+% Unique y positions per x position
+yUnique = arrayfun(@(X) unique(channelpos(channelpos(:,1)==X,2)),xUnique,'Uni',0);
 
-    % Extract piece of channel
-    channelpostmp = channelpos(TakeRows,:);
+MissingChan = nan(0,2);
+ProbablechIdx = nan(0,1);
 
-    % pattern completion
-    pattern = arrayfun(@(X) find(channelpostmp(:,1)==X,1,'first'),xUnique,'Uni',0);
-    MissingVal = xUnique(cellfun(@isempty,pattern));
-    channelpos(rowId,1) = MissingVal;
+MissIdx = find(isnan(channelpos(:,1)));
 
-    % Now for the second dimension
-    pattern = arrayfun(@(X) find(channelpostmp(:,2)==X,1,'first'),yUnique,'Uni',0);
-    usedyVals = yUnique(~cellfun(@isempty,pattern));
-    [~,id2] = ismember(channelpostmp(:,2),usedyVals);
-    if mod(rowId,2) == 0 % even; repeat old number
-        channelpos(rowId,2) = usedyVals(id2(find(id2==0)-1));
-    else % start new number
-        channelpos(rowId,2) = usedyVals(id2(find(id2==0)-1)) + ySteps;
+% Recreate a channelmap based on these values
+for xid = 1:numel(xUnique)
+    yAllPos = yUnique{xid}; % Current column
+
+    % Most similar column
+    SameIdx = find(cellfun(@(X) all(ismember(yUnique{xid},X)),yUnique)); % This column should have the same ypos
+    SameIdx(SameIdx == xid) =  [];
+    [~,Idx2] = min(abs(numel(yUnique{xid})-cellfun(@numel,yUnique(SameIdx)))); % Most similar column
+    yAllPosSim = yUnique{SameIdx(Idx2)};
+
+    if any(~ismember(yAllPosSim,yAllPos))
+        MissingChan = cat(1,MissingChan,[repmat(xUnique(xid),sum(~ismember(yAllPosSim,yAllPos)),1),yAllPosSim(~ismember(yAllPosSim,yAllPos))]);
+        TheseChs = find(chOrder==xid);
+        steps = [nan diff(TheseChs)];
+        stepsz = nanmin(steps);
+        PotentialChans = min(TheseChs)-stepsz*2:stepsz:max(TheseChs);
+        PotentialChans(PotentialChans<=0) = [];
+        ThisChans = MissIdx(ismember(MissIdx,PotentialChans));
+        if isempty(ThisChans)
+            ThisChans = MissIdx(end); % Probably the last channel?
+        end
+        ProbablechIdx = cat(1,ProbablechIdx,ThisChans);
     end
+
 end
-
-
-
-
-
+if any(arrayfun(@(X) sum(ProbablechIdx==X),unique(ProbablechIdx))>1)
+    error('Channel map filling in failed')
+end
+% Compare original and fill in missing channels
+channelpos(ProbablechIdx,:) = MissingChan;
 
 return
