1e5fa2c5745779eac0a48268a45753cff478e1aa
Enny H. van Beest <ennyvanbeest@gmail.com>
2025-04-02 16:30:15 +0100

    Revert back ExtractAndSave

--- MATLAB/UnitMatchPipeline/ExtractAndSaveAverageWaveforms.m
+++ MATLAB/UnitMatchPipeline/ExtractAndSaveAverageWaveforms.m
@@ -41,11 +41,17 @@ Currentlyloaded = 0;
 for uid = 1:nclus
     fprintf(1,'\b\b\b\b%3.0f%%',uid/nclus*100)
     if length(param.KSDir)>1 || length(param.RawDataPaths) == 1
-        tmppath = dir(fullfile(param.KSDir{GoodRecSesID(uid)},'**','RawWaveforms'));
-        tmppath = tmppath([tmppath.isdir]); % Keep only directories
+        tmppath = dir(fullfile(param.KSDir{GoodRecSesID(uid)},'**','RawWaveforms*'));
+
+        % In some Matlab versions above doesn't seem to work. Try this
+        % instead:
+        % tmppath = dir(fullfile(param.KSDir{GoodRecSesID(uid)},'**','RawWaveforms'));
+        % tmppath = tmppath([tmppath.isdir]); % Keep only directories
     else %Stitched KS
-        tmppath = dir(fullfile(param.KSDir{1},'**','RawWaveforms'));
-        tmppath = tmppath([tmppath.isdir]); % Keep only directories
+        tmppath = dir(fullfile(param.KSDir{1},'**','RawWaveforms*'));
+
+        % tmppath = dir(fullfile(param.KSDir{1},'**','RawWaveforms'));
+        % tmppath = tmppath([tmppath.isdir]); % Keep only directories
     end
     
     if length(tmppath)>1
@@ -56,7 +62,9 @@ for uid = 1:nclus
         mkdir(fullfile(param.KSDir{GoodRecSesID(uid)},'RawWaveforms'));
         tmppath = dir(fullfile(param.KSDir{GoodRecSesID(uid)},'**','RawWaveforms*'));
     end
-    Path4UnitNPY{uid} = fullfile(tmppath.folder,['Unit' num2str(AllClusterIDs(Good_Idx(uid))) '_RawSpikes.npy']); %0-indexed
+    Path4UnitNPY{uid} = fullfile(tmppath.folder,tmppath.name,['Unit' num2str(AllClusterIDs(Good_Idx(uid))) '_RawSpikes.npy']); %0-indexed
+    % If you had to change above, you may have to change this too:
+    % Path4UnitNPY{uid} = fullfile(tmppath.folder,['Unit' num2str(AllClusterIDs(Good_Idx(uid))) '_RawSpikes.npy']); %0-indexed
 
     if exist(Path4UnitNPY{uid}) && ~RedoExtraction
         continue
